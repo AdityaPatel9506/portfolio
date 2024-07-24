e159a98a17c78a12b1689c33c115d2cbfaea9964
Aditya Patel <adityapatel9506@gmail.com>
2024-07-25 01:42:29 +0530

    Add .env to .gitignore

--- js/index.js
+++ js/index.js
@@ -1,65 +1,67 @@
 const express = require("express");
 const path = require("path");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 require('dotenv').config(); // Import dotenv to load environment variables
 
 const formDir = path.join(__dirname, '../public');
-const mail = 'adityapatel9506@gmail.com';
-const formpath = path.join(__dirname, '../public/html/index.html');
+const formPath = path.join(__dirname, '../public/html/index.html');
 const form = express();
 
 form.use(express.static(formDir));
-form.use(bodyparser.urlencoded({ extended: "true" }));
+form.use(bodyParser.urlencoded({ extended: true }));
 
 form.get("/", function(req, res) {
-    res.sendFile(formpath);
+    res.sendFile(formPath);
 });
 
 form.post("/", function(req, res) {
     const firstname = req.body.firstname;
     const email = req.body.mailId;
     const query = req.body.query;
-    
+
+    // Configure the transport using environment variables
     var transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         auth: {
-            user: 'adityapatel9506@gmail.com',
-            pass: process.env.EMAIL_PASSWORD, // Use the password from the environment variable
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
         },
     });
- 
+
+    // Email to the user
     var mailOptions1 = {
-        from: "adityapatel9506@gmail.com",
+        from: process.env.FROM_EMAIL,
         to: email,
-        subject: "Thanks for contacting us, " + firstname,
-        text: "Thanks for contacting us"
+        subject: `Thanks for contacting me, ${firstname}`,
+        text: "Thanks for contacting me.",
     };
 
+    // Email to the admin
     var mailOptions2 = {
-        from: "adityapatel9506@gmail.com",
-        to: "adityapatel9506@gmail.com",
-        subject: "Aditya",
-        text: `Name: ${firstname}
-        
-        Query:
-        
-        ${query}`
+        from: process.env.FROM_EMAIL,
+        to: process.env.TO_EMAIL,
+        subject: "New Query Received",
+        text: `Name: ${firstname}\n\nQuery:\n\n${query}`,
     };
 
+    // Send email to user
     transporter.sendMail(mailOptions1, function(err, info) {
         if (err) {
-            console.log(err);
+            console.log("Error sending email to user:", err);
         } else {
-            res.redirect("/");
+            console.log("Email sent to user:", info.response);
         }
     });
 
+    // Send email to admin
     transporter.sendMail(mailOptions2, function(err, info) {
         if (err) {
-            console.log(err);
+            console.log("Error sending email to admin:", err);
         } else {
-            console.log("Query received");
+            console.log("Email sent to admin:", info.response);
+            res.redirect("/");
         }
     });
 });
---
+++ js/password.js
@@ -0,0 +1,3 @@
+pass = 'umltgrhssxolhmvx'
+
+module.exports = pass;
